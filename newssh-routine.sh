4f3a0afd7eea3fc0eed260d609a55634f4341af6
seventhsite <seventhsite@gmail.com>
2021-10-23 18:40:16 +0300

    Ssh routine script, first commit

---
+++ newssh-routine.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Get hostname
+read -p "Please, enter server address to connect: " srvaddress
+
+# Get ssh username
+while true; do
+    read -p "Do you want to connect with user \"$USER\"? Y/n: " yn
+    case $yn in
+        [Yy]* ) break;;
+        [Nn]* ) read -p "Please, enter ssh username: " sshusername ; break;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+
+# Generate ssh username@hostname
+[[ -n "$sshusername" ]] && userhost=$sshusername@$srvaddress || userhost=$srvaddress
+
+# Connect and execute
+ssh $userhost -t "sudo apt update && sudo apt -y upgrade && sudo apt dist-upgrade && sudo apt -y install mc tmux"
