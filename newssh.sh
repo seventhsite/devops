51a08a59c7a89e8c5e528016781dadb66da208f2
seventhsite <seventhsite@gmail.com>
2021-10-23 17:05:44 +0300

    Add quotes to var HOME

--- newssh.sh
+++ newssh.sh
@@ -15,20 +15,20 @@ done
 read -p "Please, enter server address to connect: " srvaddress
 
 # Check that keyfile with this name doesn't exist
-[[ -f $HOME/.ssh/$srvaddress ]] && echo "Looks like you already have the $HOME/.ssh/$srvaddress file." && read -p "Please, enter name for the keyfiles: " keyfilename
+[[ -f "$HOME"/.ssh/$srvaddress ]] && echo "Looks like you already have the "$HOME"/.ssh/$srvaddress file." && read -p "Please, enter name for the keyfiles: " keyfilename
 
 echo "We will create an alias for it."
 read -p "Please, enter the simple name of new server: " alias
 
 # Check that this alias doesn't used in ssh config
-if grep -Fxq "Host $alias " $HOME/.ssh/config
+if grep -Fxq "Host $alias " "$HOME"/.ssh/config
 then
-    echo "Looks like you already use this alias in $HOME/.ssh/config"
+    echo "Looks like you already use this alias in "$HOME"/.ssh/config"
     read -p "Please use another simple name: " alias
 fi
 
 # Create .ssh directory if doesn't exist
-[[ ! -d $HOME/.ssh ]] && echo "You don't have .ssh directory, I'll create it for you." && mkdir $HOME/.ssh && chmod 700 $HOME/.ssh
+[[ ! -d "$HOME"/.ssh ]] && echo "You don't have .ssh directory, I'll create it for you." && mkdir "$HOME"/.ssh && chmod 700 "$HOME"/.ssh
 
 # Create new variable keyname from keyfilename or srvaddress (if keyfilename is empty)
 [[ -n "$keyfilename" ]] && keyname=$keyfilename || keyname=$srvaddress
@@ -37,19 +37,19 @@ echo ""
 echo "Let's create a key pair. Password can be blank."
 echo ""
 
-ssh-keygen -f $HOME/.ssh/"$keyname"
+ssh-keygen -f "$HOME"/.ssh/"$keyname"
 
 echo ""
 echo "Now we will copy the public key to $srvaddress"
 echo ""
-ssh-copy-id -i $HOME/.ssh/"$keyname".pub "$srvaddress"
+ssh-copy-id -i "$HOME"/.ssh/"$keyname".pub "$srvaddress"
 
 # If ssh-copy-id exit with error
 if [ $? -eq 0 ]; then
     echo ""
 else
     echo "ERROR: We can't send public key to $srvaddress. Try it later by yourself."
-    echo "Simply add the content of $HOME/.ssh/$keyname.pub to $HOME/.ssh/authorized_keys on your new server"
+    echo "Simply add the content of "$HOME"/.ssh/$keyname.pub to "$HOME"/.ssh/authorized_keys on your new server"
 fi
 
 # Add alias to ssh config
@@ -57,9 +57,9 @@ fi
     echo "" 
     echo "Host $alias"
     echo "  HostName $srvaddress"
-    echo "  IdentityFile $HOME/.ssh/$keyname"
+    echo "  IdentityFile "$HOME"/.ssh/$keyname"
     echo "  User $USER"
-} >> $HOME/.ssh/config
+} >> "$HOME"/.ssh/config
 
 echo "Done!"
 echo "Try \"ssh $alias\""
