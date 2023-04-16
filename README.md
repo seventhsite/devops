f868482055ad2b7d41c6e03df3c792499cd85176
seventh <19604503+seventhsite@users.noreply.github.com>
2023-04-16 22:03:17 +0200

    Added Hetzner Storage Box mount script

--- README.md
+++ README.md
@@ -4,8 +4,15 @@ DevOPS scripts
 ### asking template.sh
 Advanced template for questions in bash scripts.
 
+### box_mount.sh
+Mounting Hetzner Storage box via SSHFS. *Change uXXXXX to your Hetzner Box username*. Other options is up to you.
+
+The script is used for fast temporary and safe mounting/unmounting of the Hetzner box. For permanent mounts, use fstab and other faster protocols (SMB/WebDAV).
+The script checks for the presence of a directory to mount, the necessary packages and keys. It can also generate ssh keys and copy them to storage.
+Root access is not needed. For convenience, the directory is mounted with the permissions of the current user.
+
 ### gitpush.sh
-Add all files, ask for message, commit it and push.
+Add all files, ask for message, commit it and push to github.
 
 ### newssh-key.sh
 Create ssh-keys, copy it to new server and create alias for ssh. It will __generate new ssh key pair for every host__. I don't think you need to use it.
---
+++ box_mount.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+
+set -e
+
+# Set mount point and options. CHANGE THE "uXXXXX" to your username. Other is optional
+HETZNER_USER="uXXXXX"
+MOUNT_POINT="$HOME/box"
+PRIVATE_KEY="$HOME/.ssh/id_rsa"
+OPTIONS="default_permissions,identityfile=$PRIVATE_KEY,noauto,_netdev,reconnect,uid=$(id -u),gid=$(id -g)"
+
+HETZNER_URL="$HETZNER_USER@$HETZNER_USER.your-storagebox.de"
+HETZNER_REMOTE_DIR="/"
+
+# Check if mount directory exists and create it if not
+if [ ! -d "$MOUNT_POINT" ]; then
+  read -p "Directory $MOUNT_POINT does not exist. Do you want to create it? (y/n) " dir
+  if [ "$dir" != "y" ]; then
+    echo "Please provide correct path to mount directory in \$MOUNT_POINT variable. Script aborted."
+    exit 1
+  else
+    mkdir "$MOUNT_POINT"
+    echo "Directory $MOUNT_POINT created"
+  fi
+fi
+
+if [ "$EUID" -eq 0 ]; then
+  read -p "It would be better not to run this script as root. Continue anyway? (y/n) " run_as_root
+  if [ "$run_as_root" != "y" ]; then
+    echo "Script aborted."
+    exit 1
+  fi
+fi
+
+# Check if mount point is already mounted and unmount if necessary
+if mount | grep -q "$MOUNT_POINT"; then
+  read -p "Box directory is already mounted. Do you want to unmount it? (y/n) " unmount
+  if [ "$unmount" == "y" ]; then
+    fusermount -u "$MOUNT_POINT"
+    echo "Box directory has been unmounted."
+    exit 0
+  else
+    echo "Nothing to do. Bye!"
+    exit 0
+  fi
+fi
+
+# Check if sshfs package is installed and install it if not
+if ! dpkg -s sshfs >/dev/null 2>&1; then
+  read -p "sshfs package is not installed. Do you want to install it? (y/n) " install
+  if [ "$install" == "y" ]; then
+    echo "Installing..."
+    sudo apt-get update
+    sudo apt-get install -y sshfs
+  else
+    echo "You need to install sshfs package. Try to use \"sudo apt install sshfs\""
+  fi
+fi
+
+# Check if private key exists
+if [ -e "$PRIVATE_KEY" ]; then
+  echo "Trying to access with $PRIVATE_KEY private key..."
+else
+  read -p "Can't find $PRIVATE_KEY private key. Do you want to create it? (y/n) " create_key
+  if [ "$create_key" == "y" ]; then
+    ssh-keygen -f $PRIVATE_KEY
+    echo "Installing your key. Please enter your Hetzner box password for user $HETZNER_USER"
+    cat $PRIVATE_KEY.pub | ssh HETZNER_URL install-ssh-key
+  else
+    echo "Continuing without private key. Please enter password for authentication."
+  fi
+fi
+
+# Mount remote directory to local mount point
+#echo "sshfs -o "$OPTIONS" "$HETZNER_URL":"$HETZNER_REMOTE_DIR" "$MOUNT_POINT""
+sshfs -o "$OPTIONS" "$HETZNER_URL":"$HETZNER_REMOTE_DIR" "$MOUNT_POINT"
+echo ""
+echo "Box directory has been mounted!"
+df -h $MOUNT_POINT
+echo ""
+#echo "Here is its content:"
+#cd $MOUNT_POINT
+#ls -la
